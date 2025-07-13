4142f8b5342876ec636cf447f3e7584d0d4a6d7d
Varnan Rathod <varnanrathod6@gmail.com>
2025-07-13 21:35:17 +0530

    Update preprocessing file

--- kafka-ml-consumer/scripts/preprocess.py
+++ kafka-ml-consumer/scripts/preprocess.py
@@ -6,12 +6,13 @@ def preprocess_data(df):
     df_encoded = pd.get_dummies(df, columns = ["location", "device_type", "transaction_type"])
 
     features = ["amount", "merchant_id", "user_id"] + \
-           [col for col in df_encoded.columns if col.startswith(("location", "device_type", "transaction_type_"))]
+               [col for col in df_encoded.columns if col.startswith(("location_", "device_type_", "transaction_type_"))]
 
     # scaler = StandardScaler()                       # To comment after first use
     scaler = joblib.load("../models/std_scaler.pkl") # To uncomment if model exists
 
-    X = scaler.fit_transform(df_encoded[features])
+    # X = scaler.fit_transform(df_encoded[features])  # To comment after first use 
+    X = scaler.transform(df_encoded[features])
     print("Data preprocessed\n")
     # joblib.dump(scaler, "std_scaler.pkl")           # To comment after first use
     # print("Standard scaler saved\n")                # To comment after first use
